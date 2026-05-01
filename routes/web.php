95b0081e6abdf71344bd337ee798b609b6e8b617
Cassio Leite <cassiodegoes@live.com>
2026-05-01 12:52:30 -0400

    added routes and request validation

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ContactController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/', [ContactController::class, 'index']);
+
+Route::resource('contacts', ContactController::class);
